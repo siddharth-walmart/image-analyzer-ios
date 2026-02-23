c4a111d27aafb858d08788c506e700ff3f6acc40
Siddharth <siddharth.sahu@walmart.com>
2026-02-23 11:29:55 -0600

    Initial Xcode project setup

---
+++ ImageAnalyzer/ImageAnalyzer/ImageAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageAnalyzerApp.swift
+//  ImageAnalyzer
+//
+//  Created by s0s08at on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct ImageAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
